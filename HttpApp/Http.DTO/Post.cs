544bbd170056ce5204b6a060f03f4eed3855710a
NDjavid <n.djavid@gmail.com>
2024-02-20 02:42:05 -0800

    Http commit

---
+++ HttpApp/Http.DTO/Post.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Common;
+using System.Dynamic;
+
+namespace Http.DTO;
+
+public class Post
+{
+  public int? userId { get;set; }
+  public int? id { get; set; }
+  public string? title { get;set; }
+  public string? body { get;set; }
+
+}
